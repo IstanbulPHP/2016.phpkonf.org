824a13646da4afd92a1f9d883f76cfd5d5c03458
guvenatbakan <guvenatbakan@gmail.com>
2016-02-23 00:12:01 +0200

    toArray filter added for natural sort

--- js/app.js
+++ js/app.js
@@ -28,6 +28,24 @@
     };
   }]);
 
+  app.filter('toArray', function () {
+    return function (obj, addKey) {
+      if (!angular.isObject(obj)) return obj;
+      if ( addKey === false ) {
+        return Object.keys(obj).map(function(key) {
+          return obj[key];
+        });
+      } else {
+        return Object.keys(obj).map(function (key) {
+          var value = obj[key];
+          return angular.isObject(value) ?
+              Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+          { $key: key, $value: value };
+        });
+      }
+    };
+  });
+
   app.controller('MainController', ['$http', '$scope', '$translate', 'ngDialog', '$rootScope', function ($http, $scope, $translate, ngDialog, $rootScope) {
 
     changeSpeakersLang($translate.preferredLanguage());
@@ -50,7 +68,7 @@
         template  : 'modal.html',
         appendTo  : '#speakers',
         controller : function ($scope) {
-          $scope.speaker = $rootScope.speakers[speaker];
+          $scope.speaker = speaker;
         },
         showClose : false
       });
